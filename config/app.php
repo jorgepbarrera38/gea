a8dbf8148e3e743b55f9c67c18a73d8546647ab1
Jorge Alexis Paz Barrera <jorgepbarrera38@gmail.com>
2018-06-06 18:59:28 -0500

    Añadido middleware laravel-https

--- config/app.php
+++ config/app.php
@@ -159,6 +159,9 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+//mios
+        \Ecrmnn\LaravelHttps\Providers\ServiceProvider::class,
+
 
     ],
 
